64ff6509e3c7cdf0648fe215e4538299d6419b99
Ken <KenIovino@gmail.com>
2011-12-12 00:07:48 -0500

    init commit

---
+++ vBulletin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  vBulletin
+//
+//  Created by Ken on 12/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
